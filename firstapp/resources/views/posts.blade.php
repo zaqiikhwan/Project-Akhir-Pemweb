15951020670bc80a68ec7f488968d505aeca034c
Muh Zaqi <zaqiikhwan22@gmail.com>
2022-07-06 18:32:31 +0700

    database seeder

--- firstapp/resources/views/posts.blade.php
+++ firstapp/resources/views/posts.blade.php
@@ -5,13 +5,20 @@
 
     @foreach ($posts as $post)
 
-        <article class="mb-5">
+        <article class="mb-5 border-bottom pb-4">
             <h2>
-                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
 
-            <p>{{ $post->excerpt }}
+            <p>
+                By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
             </p>
+
+            <p>
+                {{ $post->excerpt }}
+            </p>
+
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more..</a>
         </article>
 
     @endforeach
