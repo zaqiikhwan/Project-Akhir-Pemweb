1b9eca0bd4d5097b58eed196e774d3a27a4417ce
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-05-25 23:17:13 +0700

    Add footer

--- resources/views/user/layouts/main.blade.php
+++ resources/views/user/layouts/main.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.iconify.design/3/3.1.0/iconify.min.js"></script>
     <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <x-navbar/>
     @yield('content')
+    <x-footer/>
 </body>
 </html>
--- resources/views/user/pages/home.blade.php
+++ resources/views/user/pages/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('user.layouts.main')
 
 @section('title', 'Homepage')
 
