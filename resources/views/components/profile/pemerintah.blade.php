040e8aa80c5b3ee16c35efd06ae3e59d2dd13924
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-05-26 15:41:42 +0700

    Add profile page

---
+++ resources/views/components/profile/pemerintah.blade.php
@@ -0,0 +1,31 @@
+<div>
+    <h2 class="mb-4 text-3xl text-primary-500 font-semibold text-center">KEPALA DESA PALENGAAN LAOK <br> PERIODE 2023-2025</h2>
+    <section class="flex items-end justify-center mb-12">
+        <span>
+            <h2 class="text-3xl font-semibold text-primary-500">H. Moh. Sa’id. S.Ip</h2>
+            <p>Kepala Desa Palengaan Laok</p>
+        </span>
+        <img src="{{ asset('images/kades.jpg') }}" alt="">
+    </section>
+    <h2 class="font-medium text-primary-500 mb-4">Lambang</h2>
+    <section class="card flex p-4 items-center gap-2 justify-center mb-12">
+        <img src="{{ asset('images/pamekasan.png') }}" alt="logopamekasan">
+        <img src="{{ asset('images/logo.svg') }}" alt="logopalengaan" class="w-4/5">
+    </section>
+    <section class="flex gap-8">
+        <div class="basis-5/12">
+            <h2 class="font-medium text-primary-500 mb-4">Visi</h2>
+            <p class="card p-8">Melangkah menuju Desa Palengaan Laok yang Sejahtera, Berkelanjutan, dan Adil berdasarkan Prinsip-prinsip Agama dan Keadilan Sosial</p>
+        </div>
+        <div class="basis-7/12">
+            <h2 class="font-medium text-primary-500 mb-4">Misi</h2>
+            <ol class="card p-8">
+                <li>Sumber Daya Manusia Berkualitas untuk Kesejahteraan Bersama.</li> 
+                <li>Perekonomian Inklusif: Pertanian, Industri, dan Potensi Desa.</li> 
+                <li>Tata Kelola Pemerintahan dan Pelayanan Publik yang Responsif.</li> 
+                <li>Infrastruktur Desa Merata, Berkelanjutan, dan Menyejahterakan</li> 
+                <li>Pengelolaan Potensi Sosial, Budaya, Keagamaan, Kepemudaan, dan Perempuan untuk Masyarakat Harmonis</li> 
+            </ol>
+        </div>
+    </section>
+</div>
