f1439417178c31f4a2e26bbcd0a32eca49de7f81
rayhansyahrizal <rayhansyahrizal@gmail.com>
2023-05-27 07:13:00 +0700

    add styling to things in komunitas page

--- resources/views/components/profile/komunitas.blade.php
+++ resources/views/components/profile/komunitas.blade.php
@@ -1,61 +1,61 @@
 <div>
-    <h2 class="mb-4 text-3xl text-primary-500 font-semibold text-center">KOMUNITAS DI DESA PALENGAAN LAOK</h2>
-    <section class="flex card p-8 gap-4 relative items-center mb-4">
+    <h2 class="mb-10 text-3xl text-primary-500 font-semibold text-center">KOMUNITAS DI DESA PALENGAAN LAOK</h2>
+    <section class="flex card p-10 gap-5 relative items-center mb-4">
         <img 
             src="{{ asset('images/komunitas/bersahabat.jpg') }}" 
             alt="bersahabat"
             class="rounded-full h-52">
         <span>
-            <h2 class="text-3xl text-primary-500 font-semibold">PALENGAAN LAOK BERSAHABAT</h2>
+            <h2 class="text-3xl text-primary-500 font-semibold mb-1">PALENGAAN LAOK BERSAHABAT</h2>
             <p>Komunitas yang didirikan pada 14 April 2020 berfokus untuk menjadi media informasi utama bagi seluruh kegiatan yang terdapat di desa Palengaan Laok.</p>
         </span>
         <a  href="https://www.facebook.com/profile.php?id=100067048835508" 
-            class="btn-primary absolute right-6 bottom-6"
+            class="btn-primary absolute right-10 bottom-8 px-8"
             target="blank">
             Kunjungi
         </a>
     </section>
-    <section class="flex card p-8 gap-4 relative items-center mb-4">
+    <section class="flex card p-10 gap-4 relative items-center mb-4">
         <img 
             src="{{ asset('images/komunitas/fmbp.jpg') }}" 
             alt="fmbp"
             class="rounded-full h-52">
         <span>
-            <h2 class="text-3xl text-primary-500 font-semibold">FMBP PALENGAAN</h2>
+            <h2 class="text-3xl text-primary-500 font-semibold mb-1">FMBP PALENGAAN</h2>
             <p>Forum mahasiswa yang memiliki kesamaan asal dari desa Palengaan Laok, Pamekasan didirikan 30 November 2019 berfungsi sebagai tempat apresiasi bagi generasi muda yang berprestasi dan membanggakan.</p>
         </span>
         <a  href="https://www.facebook.com/fmpb.fmpb.3" 
-            class="btn-primary absolute right-6 bottom-6"
+            class="btn-primary absolute right-10 bottom-8 px-8"
             target="blank">
             Kunjungi
         </a>
     </section>
-    <section class="flex card p-8 gap-4 relative items-center mb-4">
+    <section class="flex card p-10 gap-4 relative items-center mb-4">
         <img 
             src="{{ asset('images/komunitas/ansor.jpg') }}" 
             alt="ansor"
             class="rounded-full h-52">
         <span>
-            <h2 class="text-3xl text-primary-500 font-semibold">PR GP ANSOR PALENGAAN LAOK</h2>
+            <h2 class="text-3xl text-primary-500 font-semibold mb-1">PR GP ANSOR PALENGAAN LAOK</h2>
             <p>Organisasi keagamaan yang terdapat di desa Palengaan Laok yang memiliki peran sebagai media informasi dan komunikasi Pimpinan Ranting dan Simpatisan Ansor Ranting Desa.</p>
         </span>
         <a  href="https://www.facebook.com/profile.php?id=100071818835155" 
-            class="btn-primary absolute right-6 bottom-6"
+            class="btn-primary absolute right-10 bottom-8 px-8"
             target="blank">
             Kunjungi
         </a>
     </section>
-    <section class="flex card p-8 gap-4 relative items-center mb-4">
+    <section class="flex card p-10 gap-4 relative items-center mb-4">
         <img 
             src="{{ asset('images/komunitas/karang-taruna.jpg') }}" 
             alt="karangtaruna"
             class="rounded-full h-52">
         <span>
-            <h2 class="text-3xl text-primary-500 font-semibold">KARANG TARUNA BHAKTI MANDIRI</h2>
+            <h2 class="text-3xl text-primary-500 font-semibold mb-1">KARANG TARUNA BHAKTI MANDIRI</h2>
             <p>Karang taruna yang terdapat di desa Palengaan Laok sebagai penopang utama dalam menyukseskan visi dan misi desa yang terwujud dalam program-program desa untuk masyarakat sekitar. </p>
         </span>
         <a  href="https://www.facebook.com/profile.php?id=100065191472560" 
-            class="btn-primary absolute right-6 bottom-6"
+            class="btn-primary absolute right-10 bottom-8 px-8"
             target="blank">
             Kunjungi
         </a>
