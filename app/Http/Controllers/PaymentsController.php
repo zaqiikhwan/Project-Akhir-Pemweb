63f2b946a40aca81feacba290165ca6a84ba90ff
Muh Zaqi <zaqiikhwan22@gmail.com>
2023-05-28 18:41:29 +0700

    refactor(midtrans): change some configurations in midtrans package

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Midtrans\Config;
 use App\Http\Controllers\Midtrans\CoreApi;
-class PaymentsController extends Controller
-{
+use Illuminate\Support\Str;
+
+class PaymentsController extends Controller {
     /**
      * Create a new controller instance.
      *
@@ -14,44 +14,37 @@ class PaymentsController extends Controller
     {
         //
     }
-public function qrisTransferCharge(Request $req)
-    {
-        try {
-            $transaction = array(
-                "payment_type" => "gopay",
-                "transaction_details" => [
-                    "gross_amount" => 10000,
-                    "order_id" => date('Y-m-dHis')
-                ],
-                "customer_details" => [
-                    "email" => "budi.utomo@Midtrans.com",
-                    "first_name" => "Azhar",
-                    "last_name" => "Ogi",
-                    "phone" => "+628948484848"
-                ],
-                "item_details" => array(
-                    [
-                        "id" => "1388998298204",
-                        "price" => 5000,
-                        "quantity" => 1,
-                        "name" => "Panci Miako"
-                    ],
-                    [
-                    "id" => "1388998298202",
+    public function qrisTransferCharge(Request $req) {
+    try {
+        $transaction = array(
+            "payment_type" => "gopay",
+            "transaction_details" => [
+                "gross_amount" => ['item_details'][0]['price'] * ['item_details'][0]['quantity'],
+                "order_id" => Str::uuid()->toString(),
+            ],
+            "customer_details" => [
+                "email" => "budi.utomo@Midtrans.com",
+                "first_name" => "Azhar",
+                "last_name" => "Ogi",
+                "phone" => "+628948484848"
+            ],
+            "item_details" => array(
+                [
+                    "id" => 'order-id: ' . Str::random(15) . time(),
                     "price" => 5000,
                     "quantity" => 1,
-                    "name" => "Ayam Geprek"
-                    ]
-                ),
-            );
-$charge = CoreApi::charge($transaction);
+                    "name" => "Panci Miako"
+                ],
+            ),
+        );
+        $charge = CoreApi::charge($transaction);
         if (!$charge) {
-            return ['code' => 0, 'messgae' => 'Terjadi kesalahan'];
+            return ['code' => 0, 'message' => 'Terjadi kesalahan'];
         }
-        return ['code' => 1, 'messgae' => 'Success', 'result' => $charge];
-        } catch (\Exception $e) {
-            return ['code' => 0, 'messgae' => 'Terjadi kesalahan'];
+            return ['code' => 1, 'message' => 'success', 'result' => $charge];
+        } catch (\Exception $e)
+        {
+            return ['code' => 0, 'message' => 'Terjadi kesalahan: '+ $e->getMessage()];
         }
     }
-            // return response()->json($charge);
 }
