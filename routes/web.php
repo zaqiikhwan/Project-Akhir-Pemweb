313dee84a3dc15d9246035848d51109e20e37dc9
abizard <abizard.03@gmail.com>
2023-05-29 16:25:36 +0700

    feat: product feature

--- routes/web.php
+++ routes/web.php
@@ -70,9 +70,5 @@ Route::get('agendas', [AgendaController::class, 'index'])->name('agenda.index')-
 
 Route::resource('products', ProductController::class)->middleware('auth');
 Route::get('products', [ProductController::class, 'index'])->name('product.index')->middleware('auth');
-<<<<<<< HEAD
-
-=======
->>>>>>> 33436d11a0256c289dc1129f1e946c8e53750539
 
 
